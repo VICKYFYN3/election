9eeb43533926b698cb7757dec0ea5ec880cb8467
VICKYFYN3 <vfokezie@gmail.com>
2025-03-25 20:41:52 +0100

    Fixed navbar responsiveness

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  base: import.meta.env.VITE_BASE_PATH || "/election",
-
+  plugins: [react()],
+  base: "/election", // Hardcoded base path
 })
